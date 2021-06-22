e1b439a851df83d52da22ba9d8025915f3a3610f
speedypard <monkey1wizard@gmail.com>
2021-06-22 23:18:38 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,13 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:speedypard/practice-vue-smallquiz.git main:gh-pages
+# git push -f git@github.com:speedypard/practice-vue-smallquiz.git main:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-# git push -f https://github.com/speedypard/practice-vue-smallquiz.git master:gh-pages
+git push -f https://github.com/speedypard/practice-vue-smallquiz.git main:gh-pages
 
 cd -
